63d08680268b7beac5dece0e6073870d3d664cd1
starlite998 <starlite998@hotmail.com>
2014-06-09 15:54:22 -0600

    Extension Methods

---
+++ Extension Methods/LikeADuck/MyStringExtensions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LikeADuck
+{
+    public static class MyStringExtensions
+    {
+        // Extension methods are static methods declared inside a static class
+        // The first parameter uses the keyword "this" and references the
+        // class or data type that we want to "extend".
+        public static string Quack(this string self)
+        {
+            return "<quack>" + self + "</quack>";
+        }
+
+        public static string Walk(this string self, int steps)
+        {
+            string result = self;
+
+            // Walk forwards or backwards....
+            if (steps < 0)
+            {
+                while (steps < 0)
+                {
+                    result = "[backstep] " + result;
+                    steps++;
+                }
+            }
+            else
+            {
+                while (steps > 0)
+                {
+                    result = result + "[step]";
+                    {
+                        result = result + " [step]";
+                        steps--;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Extension Methods/LikeADuck/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LikeADuck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = "Hello World!";
+            Console.WriteLine(message);
+            if (message.Contains("Hello"))
+                Console.WriteLine("The standard greeting example");
+
+            Console.WriteLine(message.Walk(5));
+        
+        }
+    }
+}
